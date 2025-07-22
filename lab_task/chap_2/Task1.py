b3d5120ca5ba14a4f158ba180e12276450cd4251
lutfil_kali <lutfilhadi1999@gmail.com>
2025-07-22 10:35:49 +0800

    Done Task1

---
+++ lab_task/chap_2/Task1.py
@@ -0,0 +1,9 @@
+#Task 1 
+#To create a program to ask for name, id , semester
+
+user_name =  input("\n\nEnter your name:")
+user_ID = input("Enter your ID:")
+
+#printing
+
+print("Hello " + user_name + ", your ID is " + user_ID)
