002275c670a648d5bbdf1eede7ab32d3aa8cf8fe
lutfil_kali <lutfilhadi1999@gmail.com>
2025-07-22 10:52:29 +0800

    Done Task3

---
+++ lab_task/chap_2/Task3.py
@@ -0,0 +1,12 @@
+#Task 3
+
+#Program to prompt age, height, weight 
+
+#input
+age = input("Please enter your age : ")
+height = float(input("What is your height? "))
+weight = float(input("How much do you weigh? "))
+
+#printing
+
+print("You are "+str(age)+" years old, " + str(height) + " height," +" and "+ str(weight) + " kilogram.")
