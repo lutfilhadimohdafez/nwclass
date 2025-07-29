3571f47a1fe8bc3b498bc585be43da8fbffe9cef
lutfil_kali <lutfilhadi1999@gmail.com>
2025-07-29 10:35:06 +0800

    Task 2 copied from task 1

--- lab_task/chap_3/Task2.py
+++ lab_task/chap_3/Task2.py
@@ -5,14 +5,32 @@
 #Formula is pay = hours * rate
 
 
+
+#Formula is
+#IF total working hours is more than 40, the balance after the 40 must multiply by 10  because its OT
+#BUT IF total working hours <= 40 then rate is 5
+
+
+#1)	Rewrite your previous pay computation program to give the employee for normal time working is 5 and for overtime is 10 for hourly rate for hours worked above 40 hours.
+
+
 #User Input
 employee_hours = float(input("\nEnter hours:"))
-employee_rate = float(input("Enter rate:"))
+normal_rate = 5
+ot_rate =10
 
 #calculating the gross pay
-employee_gross_pay = employee_hours * employee_rate
+#employee_gross_pay = employee_hours * employee_rate
 
+#conditional statements
+if employee_hours <= 40:
+    pay = normal_rate *employee_hours
+    print(f"Your pay is {pay}")
+else:
+    pay = normal_rate * 40 + (employee_hours-40)*10
+    print("You get extra overtime pay!")
+    print(f"Your pay is {pay} ")
 #printing
-print("Your Pay is :"+ str(employee_gross_pay))
+#print("Your Pay is :"+ str(employee_gross_pay))
 
 
