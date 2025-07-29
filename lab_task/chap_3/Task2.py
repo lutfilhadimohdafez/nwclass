d45f9a8f9aa766a51b03ada6faff1769641deaea
lutfil_kali <lutfilhadi1999@gmail.com>
2025-07-29 10:44:31 +0800

    Task 2 finished , added error handling using try and except

--- lab_task/chap_3/Task2.py
+++ lab_task/chap_3/Task2.py
@@ -1,9 +1,5 @@
 #Task 2
-#Program to prompt user for hours and rate per hour to compute
-#gross pay
-
-#Formula is pay = hours * rate
-
+#Rewrite your pay program using try and except so that your program handles non-numeric input gracefully
 
 
 #Formula is
@@ -11,26 +7,29 @@
 #BUT IF total working hours <= 40 then rate is 5
 
 
-#1)	Rewrite your previous pay computation program to give the employee for normal time working is 5 and for overtime is 10 for hourly rate for hours worked above 40 hours.
-
-
 #User Input
-employee_hours = float(input("\nEnter hours:"))
-normal_rate = 5
-ot_rate =10
+#employee_hours = float(input("\nEnter hours:"))
+#normal_rate = 5
+#ot_rate =10
 
 #calculating the gross pay
 #employee_gross_pay = employee_hours * employee_rate
 
-#conditional statements
-if employee_hours <= 40:
-    pay = normal_rate *employee_hours
-    print(f"Your pay is {pay}")
-else:
-    pay = normal_rate * 40 + (employee_hours-40)*10
-    print("You get extra overtime pay!")
-    print(f"Your pay is {pay} ")
-#printing
-#print("Your Pay is :"+ str(employee_gross_pay))
 
+#error handling
 
+try:
+    employee_hours = float(input("\nEnter hours:"))
+    normal_rate = 5
+    ot_rate = 10
+
+    #conditional statements
+    if employee_hours <= 40:
+        pay = normal_rate *employee_hours
+        print(f"Your pay is {pay}")
+    else:
+        pay = normal_rate * 40 + (employee_hours-40)*10
+        print("You get extra overtime pay!")
+        print(f"Your pay is {pay} ")
+except:
+    print("\nERROR! Please enter numbers only!")
