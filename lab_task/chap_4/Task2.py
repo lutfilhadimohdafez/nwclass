7cce35f001fbe4080a53b2d6e9ee693a4e63b2d9
lutfil_kali <lutfilhadi1999@gmail.com>
2025-08-05 11:11:36 +0800

    Created Task 2 with error handling

--- lab_task/chap_4/Task2.py
+++ lab_task/chap_4/Task2.py
@@ -1,25 +1,20 @@
 # Task 2 Chapter 4
-
 # Get functions to take input and display
-
 # Function def
-
+# Function untuk take input below:
 def info2():
     nama = str(input("\nEnter your name : "))
     umur = int(input("\nEnter your age : "))
-
     # Below return dictionary
     return {"nama": nama , "umur":umur}
-
-# Function untuk display
-
+# Function untuk display below:
 def psps(hasil):
     nama = hasil["nama"]
     umur = hasil["umur"]
     print(f"\nYour name is {nama} and you are {umur} years old.")
-
-
-#main function
-
-hasil = info2()
-psps(hasil)
+#main function below
+try:
+    hasil = info2()
+    psps(hasil)
+except:
+    print("Input error")
