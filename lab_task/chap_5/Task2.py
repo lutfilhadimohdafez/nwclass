c5d8c2368fb8424cbb1e6c165c5f5cc3cf8a367a
lutfil_kali <lutfilhadi1999@gmail.com>
2025-08-12 12:06:51 +0800

    Chap_4 task 2 done

--- lab_task/chap_5/Task2.py
+++ lab_task/chap_5/Task2.py
@@ -2,4 +2,26 @@
 
 
 # Variables
+count = 0
+total = 0
+average =0
 
+while True:
+    number = input('Enter number:')
+
+    if number == 'Finish' or number == 'finish':
+        break
+
+    try:
+        checknumber = float(number)
+    except:
+        print('Invalid input, please enter number!')
+
+    count+=1
+    total+=checknumber
+
+average = total/count
+
+print("Count\t: " + str(count))
+print("Total\t: " + str(total))
+print("Average\t:" + str(average))
