4bde978b902de2c690df7da48522af2140ba5438
lutfil_kali <lutfilhadi1999@gmail.com>
2025-08-26 12:19:24 +0800

    Task1 chap 7 finished with error handling

--- lab_task/chap_7/Task1.py
+++ lab_task/chap_7/Task1.py
@@ -1,22 +1,35 @@
 # Task 1
 
-# OPen file
-fileHandler = open("mbox-short.txt","r")
+# File name input
+# fileName = str(input("Please enter your file name : "))
 
+# Open file command
+# fileHandler = open(fileName,"r")
+
+# Error Handling
+file_error_int = 0
+while file_error_int < 3:
+    try:
+        fileName = str(input("Please enter your file name : "))
+        fileHandler = open(fileName,"r")
+        break
+    except:
+        print("File name error. Try again.")
+        file_error_int += 1
+# Exit if 3 times
+if file_error_int == 3:
+    exit()
 # Variable init
 fileLines = 0
-
 # Loop
 for line in fileHandler:
     fileLines += 1
     if line.startswith("From:"):
         print(str(fileLines) + "\t"+line.rstrip("\n"))
+    #Below to print all lines number
     #else:
         #print(str(fileLines))
-
 # Printing
 print("Total lines: " + str(fileLines))
-
-
 # Close file
 fileHandler.close()
