22a80d98505c26cfb9a737c6e0fc72ec824ae111
Lutfil Hadi <lutfilhadi1999@gmail.com>
2025-10-07 09:35:30 +0800

    oh dear

--- lab_task/chap_10/task1.py
+++ lab_task/chap_10/task1.py
@@ -1,1 +1,21 @@
 # Python lab task number 10
+
+# Error Handling
+
+
+# Nvm just do the file thing
+fileHandler = open("words.txt")
+
+# VARIABLES
+counts = dict()
+for line in fileHandler:
+    words = line.split()
+    for word in words:
+        counts[word]=counts.get(word,0)+1
+
+lst = list()
+for key,val in counts.items():
+    lst.append((val,key))
+lst.sort(reverse=True)
+for val,key in lst[:40]:
+    print(key,val)
